8252a56ab399ca933437a8b65f87ba14fd9b4510
dduckchul <dduckchul@naver.com>
2025-03-21 11:52:34 +0900

    feat : 맵 트리거 이벤트 수정 1. 리스폰 위치 조정 (모델링 다시 Export, Pivot point 수정) 2. 트리거 이벤트 설정 3. 마리오 카트 마리오 삭제(!?)

--- Assets/07_Scripts/Map/MapManager.cs
+++ Assets/07_Scripts/Map/MapManager.cs
@@ -1,30 +1,28 @@
-using System.Collections;
 using Photon.Pun;
-using Unity.VisualScripting;
 using UnityEngine;
 
-public class MapManager : MonoBehaviour
+public class MapManager : MonoBehaviourPunCallbacks
 {
     [SerializeField] private int totalLap = 3; 
     
     [Header("맵 시작 / 종료 포지션 설정")]
     public Transform[] startPos;
-    public GameObject endLine;
-    public GameObject deadZone;
-    
-    public void Start()
-    {
-        deadZone.GetComponent<Collider>();
-    }
+    public Transform[] checkPoints;
 
     // 바닥으로 떨어졌을때
-    public void OnTouchDeadZone()
+    public void OnTouchDeadZone(Collider otherKart)
     {
-        
+        Debug.Log("OnTouchDeadZone");
     }
 
     // 결승선 닿았을때 호출하는 메서드
-    public void OnTouchFinishLine(GameObject kart)
+    public void OnTouchFinishLine(Collider otherKart)
+    {
+        Debug.Log("OnTouchFinishLine");
+    }
+
+    // 맵에서 부스터 닿았을떄 메서드
+    public void OnTouchBooster(Collider otherKart)
     {
         
     }
@@ -43,7 +41,7 @@ public class MapManager : MonoBehaviour
         // isKinematic을 켰다 꺼주자 (운동 효과 제거)
         kartRigid.isKinematic = true;
         playerKart.transform.position = startingPoint.position;
-        playerKart.transform.rotation = startingPoint.rotation;
+        playerKart.transform.rotation = Quaternion.Euler(0, -175, 0);
         kartRigid.isKinematic = false;
     }
 }
--- Assets/07_Scripts/Map/MapTriggerEventScript.cs
+++ Assets/07_Scripts/Map/MapTriggerEventScript.cs
@@ -1,18 +1,22 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 [RequireComponent(typeof(Collider))]
 public class MapTriggerEventScript : MonoBehaviour
 {
+    private Collider _collider;
     private string _tag;
-
+    
+    public UnityEvent<Collider> collisionEvent;
+    
     void Awake()
     {
-    
+        _collider = GetComponent<Collider>();
         _tag = gameObject.tag;        
     }
 
     public void OnTriggerEnter(Collider other)
     {
-
+        collisionEvent.Invoke(other);
     }
 }
