04b7b2d220cb71f93b72816d8d98034150097a27
dduckchul <dduckchul@naver.com>
2025-04-02 17:07:08 +0900

    feat : 체크포인트 두배로

--- Assets/07_Scripts/Map/GameManager.cs
+++ Assets/07_Scripts/Map/GameManager.cs
@@ -1,11 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Net;
-using System.Threading;
 using Cinemachine;
 using Photon.Pun;
 using Photon.Realtime;
-using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
